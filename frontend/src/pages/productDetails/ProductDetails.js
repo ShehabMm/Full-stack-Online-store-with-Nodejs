94130b82246a1da1be84c3b31b954077d030876e
shehabMm <shehab.mm@gmail.com>
2023-03-23 19:27:06 +0200

    swiper

--- frontend/src/pages/productDetails/ProductDetails.js
+++ frontend/src/pages/productDetails/ProductDetails.js
@@ -14,64 +14,74 @@ const Productdetails = () => {
   console.log(data)
 
 
-if (data) {
-  return (
+  if (data) {
+    return (
 
 
 
-    <div className="card-products">
-      <div className="left">
-        <img
-          src={data.imageLink[2]}
-          alt="shoe"
-        />
-        <i className="fa fa-long-arrow-left"></i>
-        <i className="fa fa-long-arrow-right"></i>
-      </div>
-      <div className="right">
-        <div className="product-info">
-          <div className="product-name">
-            <h1>{data.description}</h1>
-            <i className="fa fa-search"></i>
-            <i className="fa fa-user"></i>
-            <i className="fa fa-shopping-cart"></i>
-          </div>
-          <div className="details">
-            <h3>Summer Collection</h3>
-            <h2>{data.productName}</h2>
-            <h4>
-              <span className="fa fa-dollar"></span>${data.price}
-            </h4>
-            <h4 className="dis">
-              <span className="fa fa-dollar"></span>500
-            </h4>
-          </div>
-          <ul>
-            <li>SIZE</li>
-            <li className="bg">7</li>
-            <li className="bg">8</li>
-            <li className="bg">9</li>
-            <li className="bg">10</li>
-            <li className="bg">11</li>
-          </ul>
-          <ul>
-            <li>COLOR</li>
-            <li className="yellow"></li>
-            <li className="black"></li>
-            <li className="blue"></li>
-          </ul>
-          <span className="foot">
-            <i className="fa fa-shopping-bag"></i>Buy Now
-          </span>
-          <span className="foot">
-            <i className="fa fa-shopping-cart"></i>Add TO Cart
-          </span>
+      <div className="card-products">
+        <div className="left">
+          <img
+            src={data.imageLink[2]}
+            alt="shoe"
+          />
+          <i className="fa fa-long-arrow-left"></i>
+          <i className="fa fa-long-arrow-right"></i>
         </div>
-      </div>
-    </div>
-  );
+        <div className="right">
+          <div className="product-info">
+            <div className="product-name">
+              <h1>{data.description}</h1>
+              <i className="fa fa-search"></i>
+              <i className="fa fa-user"></i>
+              <i className="fa fa-shopping-cart"></i>
+            </div>
+            <div className="details">
+              <h3>Summer Collection</h3>
+              <h2>{data.productName}</h2>
+              <h4>
+                <span className="fa fa-dollar"></span>${data.price}
+              </h4>
+              <h4 className="dis">
+                <span className="fa fa-dollar"></span>500
+              </h4>
+            </div>
+            <ul>
+              <li>SIZE</li>
+              <li className="bg">7</li>
+              <li className="bg">8</li>
+              <li className="bg">9</li>
+              <li className="bg">10</li>
+              <li className="bg">11</li>
+            </ul>
+            <ul>
+              <li>COLOR</li>
+              <li className="yellow"></li>
+              <li className="black"></li>
+              <li className="blue"></li>
+            </ul>
+            <span className="foot">
+              <i className="fa fa-shopping-bag"></i>Buy Now
+            </span>
+            <span className="foot">
+              <i className="fa fa-shopping-cart"></i>Add TO Cart
+            </span>
+          </div>
+        </div>
 
-}
+        {/* //swiper react >>>>>>>>>>>>>> */}
+
+
+
+
+      </div>
+
+
+
+
+    );
+
+  }
 
 };
 
