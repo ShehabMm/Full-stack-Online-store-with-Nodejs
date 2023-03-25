4f1b354679dc0cafa9a336479a9870399e70b2c1
shehabMm <shehab.mm@gmail.com>
2023-03-25 11:13:51 +0200

    emailjs

--- frontend/src/MUI-components/realContac/realContact.jsx
+++ frontend/src/MUI-components/realContac/realContact.jsx
@@ -3,36 +3,67 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import SendIcon from '@mui/icons-material/Send';
 import ScrollReveal from 'scrollreveal';
-
-
-
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 
 
 const RealContact = () => {
+
+  const sendEmail = (eo) => {
+
+    emailjs.sendForm('service_um2xtn9', 'template_b2vjsa5', form.current, '7LBu4kSgGpdIHFdd7')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+  };
+
+
+
+
+
+
+
+  const form = useRef();
+
+
+
+
+
   return (
-    <div className="hero">
-      <form>
+    <div className="hero" >
+      <form ref={form} >
         <div className="row">
           <div className="input-group">
-            <TextField id="" label="Name" variant="outlined" />
+            <TextField id="" label="Name" variant="outlined" name="user_name" />
           </div>
 
           <div className="input-group">
-            <TextField id="" label="Email Adress" variant="outlined" />
+            <TextField id="" label="Email Adress" variant="outlined" name="user_email" />
           </div>
         </div>
 
         <div className="input-group">
-          <TextField id="" label="Subject" variant="outlined" sx={{ width: "420px" }} />
+          <TextField id="" label="Subject" variant="outlined" sx={{ width: "420px" }} name="subject" />
         </div>
 
         <div className="input-group">
           <TextField id="" label="Message" rows={4}
-            multiline variant="filled"  sx={{border:"1px solid black"}}  />
+            multiline variant="filled" sx={{ border: "1px solid black" }} name="message" />
         </div>
 
-        <Button size="large" variant="contained" color="info" type="submit"
+        <Button
+          onClick={(eo) => {
+            eo.preventDefault()
+            sendEmail()
+
+
+
+          }}
+
+          size="large" variant="contained" color="info" type="submit"
           sx={{ display: "flex", width: "150px", marginLeft: "center", marginRight: "center" }}
           endIcon={<SendIcon />}          >
 
@@ -44,13 +75,13 @@ const RealContact = () => {
     </div>
   );
 };
-ScrollReveal({    
+ScrollReveal({
   reset: true,
-  distance:"60px",
-  duration:2500,
-  delay:400
-  
-  });
-  ScrollReveal().reveal('.hero', { delay: 500, origin:'bottom' });
+  distance: "60px",
+  duration: 2500,
+  delay: 400
+
+});
+ScrollReveal().reveal('.hero', { delay: 500, origin: 'bottom' });
 
 export default RealContact;
